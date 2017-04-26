85a0a6a71c3788508ea86f2c42ef804dd872311e
Andrey_Radzkov <liniyakamnya@gmail.com>
2017-04-26 17:43:40 +0300

    Extract validators

--- src/components/FieldLevelValidationForm.js
+++ src/components/FieldLevelValidationForm.js
@@ -4,25 +4,10 @@ import {TextInputRedux} from "../components/TextInputRedux";
 import Button from "react-bootstrap/lib/Button";
 import ButtonToolbar from "react-bootstrap/lib/ButtonToolbar";
 import Col from "react-bootstrap/lib/Col";
+import {aol, email, maxLength15, minValue18, number, required, tooOld} from "../validators/validatorsForFormat";
 
 import "../styles/styles.scss"; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 
-const required = value => value ? undefined : 'Required';
-const maxLength = max => value =>
-  value && value.length > max ? 'Must be ${max} characters or less' : undefined;
-const maxLength15 = maxLength(15);
-const number = value => value && isNaN(Number(value)) ? 'Must be a number' : undefined;
-const minValue = min => value =>
-  value && value < min ? 'Must be at least ${min}' : undefined;
-const minValue18 = minValue(18);
-const email = value =>
-  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
-    'Invalid email address' : undefined;
-const tooOld = value =>
-  value && value > 65 ? 'You might be too old for this' : undefined;
-const aol = value =>
-  value && /.+@aol\.com/.test(value) ?
-    'Really? You still use AOL for your email?' : undefined;
 
 const FieldLevelValidationForm = (props) => {
   const {handleSubmit, pristine, reset, submitting, invalid} = props;
@@ -46,7 +31,7 @@ const FieldLevelValidationForm = (props) => {
         />
         <Field name="other" type="number"
                component={TextInputRedux} label="Other" placeholder="Other"
-               validate={[required, number, minValue18]}
+               validate={[required]}
                warn={tooOld}
         />
         <Col sm={8} smOffset={4}>
