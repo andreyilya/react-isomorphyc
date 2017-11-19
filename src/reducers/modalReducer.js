57a84db2b8f32227b9b3fe2a2d530f00111ada4a
liniyakamnya <liniyakamnya@gmail.com>
2017-11-19 16:36:54 +0300

    refactoring

--- src/reducers/modalReducer.js
+++ src/reducers/modalReducer.js
@@ -1,15 +1,18 @@
 import {CLOSE_MODAL, OPEN_MODAL} from "../constants/actionTypes";
-import objectAssign from "object-assign";
 
 const modalReducer = (state = {}, action) => {
-  let assignedObject = objectAssign({}, state);
   switch (action.type) {
     case OPEN_MODAL:
-      assignedObject[action.modalId] = true;
-      return assignedObject;
-    case CLOSE_MODAL:
-      assignedObject[action.modalId] = false;
-      return assignedObject;
+      return {
+        ...state,
+        [action.modalId]: true
+      };
+    case
+    CLOSE_MODAL:
+      return {
+        ...state,
+        [action.modalId]: false
+      };
     default:
       return state;
   }
