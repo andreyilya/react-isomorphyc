b8ac7ad49d84ddbcdcd57ea595e4912ee1d7dd07
liniyakamnya <liniyakamnya@gmail.com>
2017-12-25 18:51:41 +0300

    refresh token for oauth2

--- src/pages/InfiniteScrollPage.js
+++ src/pages/InfiniteScrollPage.js
@@ -6,10 +6,9 @@ import '../styles/about-page.css';
 const InfiniteScrollPage = () => {
   return (
     <div>
-      <h2 className="alt-header">About</h2>
+      <h2 className="alt-header">Infinite scroll</h2>
       <p>
-        This example app is part of the <a href="https://github.com/coryhouse/react-slingshot">React-Slingshot
-        starter kit</a>.
+        This example app for demos. This page is for infinite scroll.
       </p>
       <p>
         <Link to="/badlink">Click this bad link</Link> to see the 404 page.
