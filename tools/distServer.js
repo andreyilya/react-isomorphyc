23f004b4e29e6f7b3ccd0fa0fa2e7ea67f7f28b3
liniyakamnya <liniyakamnya@gmail.com>
2018-01-07 13:12:51 +0300

    https. Need to import self-signed sertificate to store

--- tools/distServer.js
+++ tools/distServer.js
@@ -7,10 +7,17 @@ import webpackDevMiddleware from "webpack-dev-middleware";
 import fs from "fs";
 import compression from "compression";
 import config from "../webpack/webpack.config.prod";
+import https from 'https';
 const compiler = webpack(config);
-
 const app = new Express();
 const port = 3000;
+
+let options = {
+  key: fs.readFileSync( './tools/certificates/localhost_3000.key' ),
+  cert: fs.readFileSync( './tools/certificates/localhost_3000.cert' ),
+  requestCert: false,
+  rejectUnauthorized: false
+};
 
 app.use(compression());
 // app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
@@ -20,12 +27,13 @@ app.use(Express.static(path.resolve(__dirname, '..', 'dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'));
 });
-//TODO: enable https
+//TODO: enable https, import correct certificates
 //TODO: create proxy
-app.listen(port, function (error) {
+let server = https.createServer( options, app );
+server.listen(port, function (error) {
   if (error) {
     console.error(error);
   } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
+    console.info("==> 🌎  Listening on port %s. Open up https://localhost:%s/ in your browser.", port, port);
   }
 });
